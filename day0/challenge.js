4f535e87fcdece1632e8c17fa0254327f386c8b6
AR1ANDLE <elmoutaouakkilabdelmoughit@gmail.com>
2026-09-01 15:38:48 +0200

    Fix no Math.max usage

--- day0/challenge.js
+++ day0/challenge.js
@@ -32,7 +32,10 @@ else {console.log("F")}
 let a = 10
 let b = 60
 let c = 30
-console.log(Math.max(a, b, c))
+
+if (a > b && a > c) {console.log("a is the largest")}
+else if (b > a && b > c) {console.log("b is the largest")}
+else {console.log("c is the largest")}
 
 // Leap year checker
 
