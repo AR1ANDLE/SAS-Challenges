7e1053e25d7a83c7cf65f1c0204da5e6cb819b48
AR1ANDLE <elmoutaouakkilabdelmoughit@gmail.com>
2026-09-01 11:09:29 +0200

    Variables with let

---
+++ day0/let.js
@@ -0,0 +1,27 @@
+// Assigning variables with let 
+
+
+// day 0
+let hour = 24;
+let day;
+let name = "Moughit"
+let studying = true
+let weeks = null
+
+// day 1
+hour = 27; // can change its value
+day = 1; // can give it value later and change its data type
+let name = "moughit" // can't be reassigned
+studying = false // can be changed
+weeks = 1; // can change value alongside its data type
+
+if (hour > 24) {
+    let name = 'MOUGHIT' // can be re-assigned in its scope
+    console.log(name)
+}
+
+
+// let variables can be changed
+// Can be declared without giving them a value
+// Can be redeclared in its scope
+// can change its data type 
