47db6d788cac455247cca7e5af82bf99114478a2
AR1ANDLE <elmoutaouakkilabdelmoughit@gmail.com>
2026-09-01 11:10:21 +0200

    Variables with var

---
+++ day0/var.js
@@ -0,0 +1,29 @@
+// assigning variables with var
+
+
+// day 0
+var hour = 24;
+var day;
+var name = "Moughit"
+var studying = true
+var weeks = null
+
+// day 1
+hour = 27; // can change its value
+day = 1; // can give it value later and change its data type
+var name = "moughit" // can be reassigned
+studying = false // can be changed
+weeks = 1; // can change value alongside its data type
+
+if (hour > 24) {
+    // var name = 'MOUGHIT' // can be re-assigned in its scope
+    console.log(name)
+}
+
+// var name = "MOUGHIT" ? hour > 24 // can't be reassigned with the ternary operator
+
+
+// var variables can be changed
+// Can be declared without giving them a value
+// Can be redeclared in its scope
+// can change its data type 
