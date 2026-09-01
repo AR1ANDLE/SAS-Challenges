a518ebc9e1db2d034fc166dd70bf72b05b34fa7d
AR1ANDLE <elmoutaouakkilabdelmoughit@gmail.com>
2026-09-01 11:10:57 +0200

    Variables with const

---
+++ day0/const.js
@@ -0,0 +1,8 @@
+// Assigning variables with const
+
+
+const hour = 24; // can never be changed
+const day; // can't declare it without assigning a value
+const name = "Moughit" // can't be changed
+const weeks = 4; // can't be changed or reassigned
+const hour; // can't redeclare it
